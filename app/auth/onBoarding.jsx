b487b3254f614b8bf8b04fe37702e55bed21f4b3
Itzhak Rouach <tzachiruash12345@gmail.com>
2026-01-03 01:26:55 +0200

    done editing and designing the onboarding page view

--- app/auth/onBoarding.jsx
+++ app/auth/onBoarding.jsx
@@ -1,13 +1,108 @@
 import { useRouter } from "expo-router";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Image, StyleSheet, View } from "react-native";
+import { Button, Text } from "react-native-paper";
 
 export default function OnBoardingScreen() {
   //set navigation so can navigate to the correct View
   const router = useRouter();
   return (
-    <View>
-      <Text>This is onboarding screen</Text>
+    <View style={styles.container}>
+      <View style={styles.contentWrapper}>
+        <Image
+          source={require("../../assets/images/GuardAppIcon.png")}
+          style={styles.icon}
+        />
+        <Text variant="headlineLarge" style={styles.title}>
+          Guard<Text style={styles.titleSec}>Pay</Text>
+        </Text>
+        <Text variant="bodyLarge" style={styles.intro}>
+          Stop worrying about missing hours or incorrect paychecks. GuardPay
+          tracks every shift with precision, ensuring you get exactly what you
+          earned, every single time.
+        </Text>
+      </View>
+      <View style={styles.btnContainer}>
+        <Button
+          type="contained"
+          labelStyle={styles.btnLabel}
+          style={styles.btn}
+          buttonColor="#E2EAF2"
+          textColor="#213448"
+          onPress={() => router.replace("auth/signIn")}
+        >
+          Sign In
+        </Button>
+        <Button
+          type="outlined"
+          labelStyle={styles.btnLabel}
+          style={styles.secondeBtn}
+          textColor="#213448"
+          onPress={() => router.replace("auth/register")}
+        >
+          Create Account
+        </Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F8FAFC",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 60,
+  },
+  contentWrapper: {
+    alignItems: "center",
+    paddingHorizontal: 40,
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 32,
+    letterSpacing: -1,
+    fontWeight: 700,
+    color: "#213448",
+  },
+  titleSec: {
+    color: "#5e98d7ff",
+    fontWeight: 700,
+  },
+  intro: {
+    color: "#213448",
+    fontWeight: 400,
+    lineHeight: 24,
+    marginTop: 15,
+    opacity: 0.7,
+    fontSize: 16,
+    textAlign: "justify",
+  },
+  btnContainer: {
+    flex: 1,
+    gap: 12,
+    marginTop: 80,
+    paddingHorizontal: 20,
+    width: "100%",
+  },
+  btnLabel: {
+    borderRadius: 1,
+    fontWeight: 500,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    fontSize: 18,
+    letterSpacing: 0.5,
+    borderColor: "#E2E8F0",
+  },
+
+  icon: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+
+  secondeBtn: {
+    borderColor: "rgba(33, 52, 72, 0.5)",
+    borderWidth: 1,
+  },
+});
